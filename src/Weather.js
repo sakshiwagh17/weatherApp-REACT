3429c573ed9b21f437af717a314d9ec109861812
Sakshi Wagh <sakshikwagh17@gmail.com>
2025-01-13 22:44:56 +0530

    Initial commit

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import CurrentLocation from './CurrentLocation';
+import axios from 'axios';
 
 const Weather = () => {
 
-    const [InputCity, setInputCity] = useState('');
+    const [InputCity, setInputCity] = useState();
     const [weather, setWeather] = useState({});
     const handleChange = (e) => {
         const inputval = e.target.value;
@@ -12,14 +13,14 @@ const Weather = () => {
     const fetchWether = async () => {
         try {
 
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${InputCity}&appid=${'8742b32a5fbb68977297f9b329513461'}`)
-            const result = await response.json();
-            console.log(result);
-            setWeather(result);
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${InputCity}&appid=${'8742b32a5fbb68977297f9b329513461'}`)
+
+            console.log(response);
+            setWeather(response);
             setInputCity('');
 
         } catch (error) {
-            console.log(error);
+            console.log("Error to fetching data", error);
 
         }
     }
@@ -30,38 +31,38 @@ const Weather = () => {
         <div>
             <input placeholder='Enter city' value={InputCity} type='text' onChange={handleChange} />
             <button className='btn' onClick={handleSubmit}>Get Weather</button>
-            {weather.main?(
+            {weather.main ? (
                 <div className='weather-layout'>
 
-                <div className="weather-container">
-                    <h1>{weather.name},{weather.sys.country}</h1>
-                    <p></p>
-                    <p>{<CurrentLocation />}</p>
-                    <h1>{Math.round(weather.main.temp - 273.15)}°C</h1>
+                    <div className="weather-container">
+                        <h1>{weather.name},{weather.sys.country}</h1>
+                        <p></p>
+                        <p>{<CurrentLocation />}</p>
+                        <h1>{Math.round(weather.main.temp - 273.15)}°C</h1>
+                    </div>
+                    <div className='weather-container2'>
+                        <input placeholder='Enter city' value={InputCity} type='text' onChange={handleChange} />
+                        <button className='btn' onClick={handleSubmit}>Get Weather</button>
+                        <h2>{weather.name}</h2>
+                        <p>Tempture : {Math.round(weather.main.temp - 273.15)}°C</p>
+                        <p>Humidity {weather.humidity}%</p>
+                        <p>Visibility {weather.visibility} mi</p>
+                        <p>Wind Speed {weather.wind.speed}km/h</p>
+                    </div>
                 </div>
-                <div className='weather-container2'>
-                    <input placeholder='Enter city' value={InputCity} type='text' onChange={handleChange} />
-                    <button className='btn' onClick={handleSubmit}>Get Weather</button>
-                    <h2>{weather.name}</h2>
-                    <p>Tempture : {Math.round(weather.main.temp - 273.15)}°C</p>
-                    <p>Humidity {weather.humidity}%</p>
-                    <p>Visibility {weather.visibility} mi</p>
-                    <p>Wind Speed {weather.wind.speed}km/h</p>
-                </div>
-            </div>
 
-             ):(
+            ) : (
                 <div>
-                    {Object.keys(weather).length===0?(
+                    {Object.keys(weather).length === 0 ? (
                         <p>Enter the vaild city to check and press "Get weather" to fetch data.</p>
-                    ):(
+                    ) : (
                         <p>Unable to check data please try again..</p>
                     )}
                 </div>
-             )} 
+            )}
         </div>
-   
-  )
+
+    )
 }
 
 export default Weather
